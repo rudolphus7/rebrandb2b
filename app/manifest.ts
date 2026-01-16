b4329d79c9f2337b5ca4ac26d5f0508befa638e5
REBRAND ADMIN <rebrand.com.ua@gmail.com>
2026-01-16 23:10:54 +0200

    дизайн X

---
+++ app/manifest.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: 'Rebrand Studio B2B',
+        short_name: 'Rebrand',
+        description: 'Wholesale portal for Rebrand Studio',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#000000',
+        theme_color: '#000000',
+        icons: [
+            {
+                src: '/favicon1.ico',
+                sizes: 'any',
+                type: 'image/x-icon',
+            },
+        ],
+    };
+}
